ffd52b860b85fff1474e57ac40a78445e9aed55d
Brandon Sansone <bsansone@users.noreply.github.com>
2018-11-12 23:53:08 -0600

    Updates

---
+++ precache-manifest.5bd2403223cf9935b1f05b35b0a055a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "738a16a178d39df74782",
+    "url": "/sudoku/static/js/runtime~main.738a16a1.js"
+  },
+  {
+    "revision": "a3ce7f9fc5e99c4f695d",
+    "url": "/sudoku/static/js/main.a3ce7f9f.chunk.js"
+  },
+  {
+    "revision": "d9e044c87b1809a4737b",
+    "url": "/sudoku/static/js/1.d9e044c8.chunk.js"
+  },
+  {
+    "revision": "a3ce7f9fc5e99c4f695d",
+    "url": "/sudoku/static/css/main.5cf14d99.chunk.css"
+  },
+  {
+    "revision": "dd287a99ffdf11e2c67cd31226ef35f4",
+    "url": "/sudoku/index.html"
+  }
+];
